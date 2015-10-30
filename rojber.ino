a173f6283d0762218d65559df9bd601dc61fdb7d
Mariusz Gruszka <my1golden@gmail.com>
2015-10-30 23:21:17 +0100

    Create rojber.ino

---
+++ rojber.ino
@@ -0,0 +1,10 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "WiFiName";
+const char* password = "WiFiPassword";
+
+WiFiServer server(80);
+
+void setup() {
+  pinMode(14, OUTPUT);
+  digitalWrite(14, 0);
